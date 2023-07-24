568426c3a1fe89d1b54a4207cc8a1ee65e52e6ea
Alice <foralice06@mail.ru>
2023-07-24 10:50:35 +0500

    HW11

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
+}
