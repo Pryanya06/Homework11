372510f979b4149f49f838a572f360949b28ce20
Alice <foralice06@mail.ru>
2023-07-24 11:46:27 +0500

    HW11

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,61 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
-        task1();
-        task2();
-        task3();
+        leapYear(2020);
+        leapYear(2023);
+        leapYear(2024);
+        task2(0, 2023);
+        task2(1, 2022);
+
+        task3 (95);
+        task3 ( 110);
     }
 
-    public static void task1() {
-        System.out.println("Задача 1");
-
+    public static void leapYear (int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 ==0) {
+            System.out.println( year + " - високосный год");
+        } else {
+            System.out.println( year + " - не високосный год");
+        }
     }
 
-    public static void task2() {
-        System.out.println("Задача 2");
+    public static void task2(int mobileOS, int mobileyear) {
+        int currentYear = LocalDate.now().getYear();
+        String mobileOSName;
 
+        switch (mobileOS) {
+            case 0: mobileOSName = "IOS";
+            break;
+            case 1: mobileOSName = " Android";
+            break;
+            default: mobileOSName = " Неизвестная ОС";
+        }
+        if (mobileyear != currentYear) {
+            System.out.println(" Для ОС " + mobileOSName + " необходимо установить lite версию");
+        } else {
+            System.out.println( " Для ОС " + mobileOSName + " необходимо установить обычную версию");
+        }
     }
 
-    public static void task3() {
-        System.out.println("Задача 3");
+    public static int task1(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if ( distance <= 60) {
+            return 2;
+        } else if ( distance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+    public static void task3 ( int distance) {
+        int days = task1(distance);
+        if ( days > 0) {
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставка не осуществляется");
+        }
     }
 }
